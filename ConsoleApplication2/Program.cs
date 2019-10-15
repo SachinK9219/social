10ff1a0808d9e5b309ad03f732fef1bff05ec8b5
pravinpadolv <prp.vad@gmail.com>
2019-10-15 23:19:51 +0530

    Project files added

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+        public void GetMethod()
+        {
+
+            List<int> ser = new List<int>();
+            for (int i = 3; i < 9; i++)
+            {
+                if(i%2==0)
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
